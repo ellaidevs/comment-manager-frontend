47f0431f3a0450f0489909efb41401cfef0e2420
Ellai <ellaiyarasan@intrack.com.my>
2021-07-25 23:43:10 +0800

    feat: app web view grid

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<header className="header">*/}
+            {/*</header>*/}
+            {/*<aside></aside>*/}
+            {/*<main></main>*/}
+            {/*<section></section>*/}
+            {/*<footer></footer>*/}
+            <div id="content">
+
+                <header>header</header>
+                <aside>aside</aside>
+                <main>main</main>
+                <section>section</section>
+                <footer>footer</footer>
+
+                {/*<div className="one">1</div>*/}
+                {/*<div className="two">2</div>*/}
+                {/*<div className="three">3</div>*/}
+                {/*<div className="four">4</div>*/}
+                {/*<div className="five">5</div>*/}
+                {/*<div className="six">6</div>*/}
+            </div>
+        </div>
+    );
 }
 
 export default App;
