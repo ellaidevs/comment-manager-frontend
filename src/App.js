e3e16ac0389bd7c0bec97955af9e2ba1e8704931
Ellai <ellaiyarasan@intrack.com.my>
2021-07-26 00:47:33 +0800

    feat: add AppBar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
     return (
         <div className="App">
-            {/*<header className="header">*/}
-            {/*</header>*/}
-            {/*<aside></aside>*/}
-            {/*<main></main>*/}
-            {/*<section></section>*/}
-            {/*<footer></footer>*/}
             <div id="content">
-
-                <header>header</header>
+                <Header/>
                 <aside>aside</aside>
                 <main>main</main>
                 <section>section</section>
                 <footer>footer</footer>
-
-                {/*<div className="one">1</div>*/}
-                {/*<div className="two">2</div>*/}
-                {/*<div className="three">3</div>*/}
-                {/*<div className="four">4</div>*/}
-                {/*<div className="five">5</div>*/}
-                {/*<div className="six">6</div>*/}
             </div>
         </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from './AppBar';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <AppBar/>
+        </div>
+    );
+};
+
+export default Header;
